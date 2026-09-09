2b4334bb0ed626525fe75f20ca1eae62577a3d98
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-09 18:19:27 +0530

    Commit-090926-4

---
+++ nodes/TARGET-wrk_customer_union.sql
@@ -0,0 +1,23 @@
+@id("210cfd42-d503-4307-a52d-5a294b502612")
+@nodeType("708")
+SELECT
+     `c_custkey` AS `c_custkey`,
+     `c_name` AS `c_name`,
+     `c_address` AS `c_address`,
+     `c_nationkey` AS `c_nationkey`,
+     `c_phone` AS `c_phone`,
+     `c_acctbal` AS `c_acctbal`,
+     `c_mktsegment` AS `c_mktsegment`,
+     `c_comment` AS `c_comment`
+FROM {{ ref('SRC2', 'customer1') }} `customer1`
+UNION
+SELECT
+     `c_custkey` AS `c_custkey`,
+     `c_name` AS `c_name`,
+     `c_address` AS `c_address`,
+     `c_nationkey` AS `c_nationkey`,
+     `c_phone` AS `c_phone`,
+     `c_acctbal` AS `c_acctbal`,
+     `c_mktsegment` AS `c_mktsegment`,
+     `c_comment` AS `c_comment`
+FROM {{ ref('SRC2', 'customer2') }} `customer2`
