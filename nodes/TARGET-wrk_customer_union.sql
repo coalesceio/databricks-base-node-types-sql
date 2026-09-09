60339d5d2672564dc9b9298a60ddf9b9f1ac54a2
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-09 18:28:57 +0530

    Commit-090926-5

--- nodes/TARGET-wrk_customer_union.sql
+++ nodes/TARGET-wrk_customer_union.sql
@@ -1,6 +1,6 @@
 @id("210cfd42-d503-4307-a52d-5a294b502612")
 @nodeType("708")
-SELECT
+(SELECT
      `c_custkey` AS `c_custkey`,
      `c_name` AS `c_name`,
      `c_address` AS `c_address`,
@@ -20,4 +20,5 @@ SELECT
      `c_acctbal` AS `c_acctbal`,
      `c_mktsegment` AS `c_mktsegment`,
      `c_comment` AS `c_comment`
-FROM {{ ref('SRC2', 'customer2') }} `customer2`
+FROM {{ ref('SRC2', 'customer2') }} `customer2`)
+where c_custkey > 2
