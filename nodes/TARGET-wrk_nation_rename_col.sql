ed59f80fd686a1bdf3588c5fa85380025578d9e6
Pranjal Mane <pranjal.mane.contractor@coalesce.io>
2026-09-11 18:24:23 +0530

    Undeployment

--- nodes/TARGET-wrk_nation_rename_col.sql
+++
@@ -1,9 +0,0 @@
-@id("4b513e66-73af-4f8b-8b07-ef613121d8b5")
-@nodeType("708")
-SELECT
-     `n_nationkey` AS `n_nationkey`,
-     `n_name` AS `n_name`,
-     `n_regionkey` AS `n_regionkey`,
-     `n_comment` AS `n_comment_new1`,
-     `timestamp_col` AS `timestamp_col`
-FROM {{ ref('SRC', 'nation') }} `nation`
