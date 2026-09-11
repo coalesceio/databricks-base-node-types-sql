f883dc8b1127649722348e04b8727b280d800579
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-11 14:30:17 +0530

    Redeployment - DEFAULT

--- nodes/TARGET-wrk_nation_table.sql
+++ nodes/TARGET-wrk_nation_table.sql
@@ -10,6 +10,6 @@ SELECT
      `n_regionkey` AS `n_regionkey_1` @description("<text>"),
      `n_comment` AS `n_comment` @defaultValue("'NA'"),
      `timestamp_col` AS `timestamp_col`,
-     `n_comment` AS `n_comment_1` @defaultValue("'NA'")  @description("<text>")
+     `n_comment` AS `n_comment_2` @defaultValue("'NA'") @description("<text>")
 FROM {{ ref('SRC2', 'nation') }} `nation`
 where n_nationkey = {{ parameters.nation }}
