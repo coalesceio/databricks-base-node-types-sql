b4e33c782182923d1be4cb1c7d683a3111a0d2d4
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-11 15:31:24 +0530

    DT

--- nodes/SRC2-wrk_nation_table.sql
+++ nodes/SRC2-wrk_nation_table.sql
@@ -5,12 +5,17 @@
 @preSQL("SELECT 1")
 @tests("SELECT 1")
 @postSQL("SELECT 1")
-SELECT
-     `n_nationkey` AS `n_nationkey` @description("<text-mod>")  @defaultValue("10"),
+SELECT ALL
+     CAST(`n_nationkey` AS INTEGER) AS `n_nationkey` @description("<text-mod>")  @defaultValue("10"),
      `n_name` AS `n_name`  @not_null @empty @notNull,
      `n_regionkey` AS `n_regionkey_1` @description("<text>"),
      `n_comment` AS `n_comment` @defaultValue("'NA'"),
      `timestamp_col` AS `timestamp_col`,
-     `n_comment` AS `n_comment_MOD` @defaultValue("'NA'")
+     `n_comment` AS `n_comment_MOD` @defaultValue("'NA'"),
+     CAST(
+     SHA1(
+          IFNULL(CAST(`n_nationkey` AS STRING), 'null') || '||' || IFNULL(CAST(`n_regionkey` AS STRING), 'null')
+     ) AS STRING
+     ) AS `hash_key_sha1_default`
 FROM {{ ref('SRC2', 'nation') }} `nation`
-where n_nationkey = 3
+where n_nationkey = {{ parameters.nation }}
