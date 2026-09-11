f01f6294b26dc35e7f5fec4e1cf2f47a4be5038e
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-11 16:49:52 +0530

    LONG -> BIGINT

--- nodes/TARGET-wrk_customer.sql
+++ nodes/TARGET-wrk_customer.sql
@@ -1,7 +1,7 @@
 @id("84256344-cec0-4ecc-ab11-4ae90714d011")
 @nodeType("708")
 SELECT
-     `c_custkey` AS `c_custkey`,
+     CAST(`c_custkey` AS BIGINT) AS `c_custkey`,
      `c_name` AS `c_name`,
      `c_address` AS `c_address`,
      `c_nationkey` AS `c_nationkey`,
