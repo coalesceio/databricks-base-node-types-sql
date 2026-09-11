8730c252e6e9f481fcc0669a2cf540b3183e0b70
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-11 16:52:24 +0530

    commit-new env-deploment 6

--- nodes/TARGET-wrk_nat_new.sql
+++ nodes/TARGET-wrk_nat_new.sql
@@ -1,7 +1,7 @@
 @id("26d95bd3-f4da-48d7-ac0e-98beb0a21292")
 @nodeType("708")
 SELECT
-     `n_nationkey` AS `n_nationkey`,
+     CAST(`n_nationkey` AS BIGINT) AS `n_nationkey`,
      `n_name` AS `n_name`,
      `n_regionkey` AS `n_regionkey`,
      `n_comment` AS `n_comment`
