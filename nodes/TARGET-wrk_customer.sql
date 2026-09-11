2473fb7af0f04cc608fa74f68cff97252bc837e6
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-09-11 17:11:56 +0530

    deployment

--- nodes/TARGET-wrk_customer.sql
+++ nodes/TARGET-wrk_customer.sql
@@ -4,7 +4,7 @@ SELECT
      CAST(`c_custkey` AS BIGINT) AS `c_custkey`,
      `c_name` AS `c_name`,
      `c_address` AS `c_address`,
-     `n_nationkey` AS `c_nationkey`,
+     `c_nationkey` AS `c_nationkey`,
      `c_phone` AS `c_phone`,
      `c_acctbal` AS `c_acctbal`,
      `c_mktsegment` AS `c_mktsegment`,
