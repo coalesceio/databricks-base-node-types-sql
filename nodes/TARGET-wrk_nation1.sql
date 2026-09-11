92f672e69d75f165bf191c19c81a4deb44d1b94e
Anandhi <anandhi.sekaran@coalesce.io>
2026-09-11 16:14:09 +0530

    COMMIT-20260911-environment updated-initial deployment2

--- nodes/TARGET-wrk_nation1.sql
+++
@@ -1,9 +0,0 @@
-@id("f70c6070-b8a3-452e-b824-5c866bd1d50a")
-@nodeType("708")
-SELECT
-     `n_nationkey` AS `n_nationkey`,
-     `n_name` AS `n_name`,
-     `n_regionkey` AS `n_regionkey`,
-     `n_comment` AS `n_comment`,
-     `timestamp_col` AS `timestamp_col`
-FROM {{ ref('SRC', 'nation') }} `nation`
